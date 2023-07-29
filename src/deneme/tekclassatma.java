8cc6274eff0cbc3262650d51153ce9ecee86aaab
MEHMETKAHRMANTR <mehmetkahraman.2028@gmail.com>
2023-07-29 19:58:20 +0300

    versiyon2

--- src/deneme/tekclassatma.java
+++ src/deneme/tekclassatma.java
@@ -1,7 +1,12 @@
 package deneme;
 
+import java.util.Scanner;
+
 public class tekclassatma {
     public static void main(String[] args) {
         System.out.println("tek class atma");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("denemeler");
+        String x= scanner.nextLine();
     }
 }
